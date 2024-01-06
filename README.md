74d2152480e4cd4aff93ca348ec0fb9bae0aa0f2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-01-06 23:50:55 +0100

    vermeer: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: 23113RKC6C Script version: 3.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Redmi 23113RKC6C (vermeer)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_vermeer-user
+add_lunch_combo omni_vermeer-userdebug
+add_lunch_combo omni_vermeer-eng
